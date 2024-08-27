eb5e20b407dadf661c137a9fe89986dfb7e63c74
Seunghee Han <sisifhro@kaist.ac.kr>
2024-08-27 14:11:56 +0900

    seunghh_240827

--- exit/modules/model.py
+++ exit/modules/model.py
@@ -8,6 +8,8 @@ from pytorch_lightning import LightningModule
 from exit.modules import heads
 from exit.modules.visiontransformer import VisionTransformer1D
 from exit.modules.utils import Normalizer, init_weights
+from exit.modules.utils import compute_pv_loss, compute_sa_loss,compute_regression_loss, compute_classification_loss
+
 
 
 from sklearn.metrics import r2_score, mean_squared_error, mean_absolute_error
@@ -31,6 +33,7 @@ class MultiModal(LightningModule):
         self.hidden_dim = config['model']['hidden_dim']
         self.exclude_keys = ['xrd', 'ref', 'name']
         self.current_tasks = []
+        self.write_log = True
         ##################################################
         # set mofid transformer (JW)
         self.mofid_transformer = None
@@ -54,11 +57,15 @@ class MultiModal(LightningModule):
             self.pv_head = heads.PVHead(self.hidden_dim)
             self.pv_head.apply(init_weights)
             self.current_tasks.append('pv')
+            self.pv_mean = config['pv_mean']
+            self.pv_std = config['pv_std']
             
         if config["loss_names"]["sa"] > 0:
             self.sa_head = heads.SAHead(self.hidden_dim)
             self.sa_head.apply(init_weights)
             self.current_tasks.append('sa')
+            self.sa_mean = config['sa_mean']
+            self.sa_std = config['sa_std']            
 
         if config["loss_names"]["mofid"] > 0:
             self.mofid_head = heads.MOFidHead(self.hidden_dim)
@@ -69,6 +76,8 @@ class MultiModal(LightningModule):
             self.regression_head = heads.RegressionHead(self.hidden_dim)
             self.regression_head.apply(init_weights)
             self.current_tasks.append('regression')
+            self.regression_mean = config['regression_mean']
+            self.regression_std = config['regression_std']             
 
         if config["loss_names"]["classification"] > 0:
             self.classification_head = heads.ClassificationHead(self.hidden_dim)
@@ -157,4 +166,31 @@ class MultiModal(LightningModule):
         if not len(self.current_tasks):
             return losses
 
+        if 'pv' in self.current_tasks:
+            normalizer = Normalizer(self.pv_mean, self.pv_std, self.device)
+            losses.update(compute_pv_loss(self, results, normalizer))
 
+        if 'sa' in self.current_tasks:
+            normalizer = Normalizer(self.sa_mean, self.sa_std, self.device)
+            losses.update(compute_sa_loss(self, results, normalizer))
+
+##################################################
+        # mofid loss JW
+        if 'mofid' in self.current_tasks:
+            pass
+##################################################
+
+        
+        if 'regression' in self.current_tasks:
+            normalizer = Normalizer(self.regression_mean, self.regression_std, self.device)
+            losses.update(compute_regression_loss(self, results, normalizer))
+
+        if 'classification' in self.current_tasks:
+            losses.update(compute_classification_loss(self, results))
+
+        return losses
+
+
+    def training_step(self, batch, batch_idx):
+        output = self(batch)
+        #total_loss = 
--- exit/modules/utils.py
+++ exit/modules/utils.py
@@ -1,5 +1,7 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
+from torchmetrics.functional import mean_absolute_error
 from torch.optim import AdamW
 from transformers import (
     get_polynomial_decay_schedule_with_warmup,
@@ -19,10 +21,143 @@ def init_weights(module):
     if isinstance(module, nn.Linear) and module.bias is not None:
         module.bias.data.zero_()
 
+#===================== loss =====================
+def compute_pv_loss(module, results, normalizer):
+    logits = module.pv_head(results['cls_feats'])
+    labels = torch.FloatTensor(results['pv']).to(logits.device)
 
-def set_schedule(pl_module):
-    lr = pl_module.hparams.config["learning_rate"]
-    wd = pl_module.hparams.config["weight_decay"]
+     # normalize encode if config["mean"] and config["std], else pass
+    logits = logits.squeeze(-1)
+    labels = normalizer.encode(labels)
+    loss = F.mse_loss(logits, labels)
+
+    labels = labels.to(torch.float32)
+    logits = logits.to(torch.float32)
+
+    results =  {
+        'pv_loss': loss,
+        'pv_logits': normalizer.decode(logits), 
+       'pv_labels':  normalizer.decode(labels),
+           }
+
+    # call update() loss and acc
+    phase = "train" if module.training else "val"
+    loss = getattr(module, f"{phase}_pv_loss")(results["pv_loss"])
+    mae = getattr(module, f"{phase}_pv_mae")(
+        mean_absolute_error(results["pv_logits"], results["pv_labels"])
+    )
+
+    if module.write_log:
+        module.log(f"pv/{phase}/loss", loss, sync_dist=True)
+        module.log(f"pv/{phase}/mae", mae, sync_dist=True)    
+
+    return results
+
+
+
+
+def compute_sa_loss(module, results, normalizer):
+    logits = module.sa_head(results['cls_feats'])
+    labels = torch.FloatTensor(results['sa']).to(logits.device)
+
+     # normalize encode if config["mean"] and config["std], else pass
+    logits = logits.squeeze(-1)
+    labels = normalizer.encode(labels)
+    loss = F.mse_loss(logits, labels)
+
+    labels = labels.to(torch.float32)
+    logits = logits.to(torch.float32)
+
+    results =  {
+        'sa_loss': loss,
+        'sa_logits': normalizer.decode(logits), 
+       'sa_labels':  normalizer.decode(labels),
+           }
+
+    # call update() loss and acc
+    phase = "train" if module.training else "val"
+    loss = getattr(module, f"{phase}_sa_loss")(results["sa_loss"])
+    mae = getattr(module, f"{phase}_sa_mae")(
+        mean_absolute_error(results["sa_logits"], results["sa_labels"])
+    )
+
+    if module.write_log:
+        module.log(f"sa/{phase}/loss", loss, sync_dist=True)
+        module.log(f"sa/{phase}/mae", mae, sync_dist=True)    
+
+    return results
+
+def compute_regression_loss(module, results, normalizer):
+    logits = module.regression_head(results['cls_feats'])
+    labels = torch.FloatTensor(results['regression']).to(logits.device)
+
+     # normalize encode if config["mean"] and config["std], else pass
+    logits = logits.squeeze(-1)
+    labels = normalizer.encode(labels)
+    loss = F.mse_loss(logits, labels)
+
+    labels = labels.to(torch.float32)
+    logits = logits.to(torch.float32)
+
+    results =  {
+        'regression_loss': loss,
+        'regression_logits': normalizer.decode(logits), 
+       'regression_labels':  normalizer.decode(labels),
+           }
+
+    # call update() loss and acc
+    phase = "train" if module.training else "val"
+    loss = getattr(module, f"{phase}_regression_loss")(results["regression_loss"])
+    mae = getattr(module, f"{phase}_regression_mae")(
+        mean_absolute_error(results["regression_logits"], results["regression_labels"])
+    )
+
+    if module.write_log:
+        module.log(f"regression/{phase}/loss", loss, sync_dist=True)
+        module.log(f"regression/{phase}/mae", mae, sync_dist=True)    
+
+    return results
+
+
+def compute_classification_loss(module, results):
+    infer = module.infer(batch)
+
+    logits, binary = module.classification_head(
+        infer["cls_feats"]
+    )  # [B, output_dim]
+    labels = torch.LongTensor(results["classification"]).to(logits.device)  # [B]
+    assert len(labels.shape) == 1
+    if binary:
+        logits = logits.squeeze(dim=-1)
+        loss = F.binary_cross_entropy_with_logits(input=logits, target=labels.float())
+    else:
+        loss = F.cross_entropy(logits, labels)
+
+    results = {
+        "classification_loss": loss,
+        "classification_logits": logits,
+        "classification_labels": labels,
+    }
+
+    # call update() loss and acc
+    phase = "train" if module.training else "val"
+    loss = getattr(module, f"{phase}_classification_loss")(
+        results["classification_loss"]
+    )
+    acc = getattr(module, f"{phase}_classification_accuracy")(
+        results["classification_logits"], results["classification_labels"]
+    )
+
+    if module.write_log:
+        module.log(f"classification/{phase}/loss", loss, sync_dist=True)
+        module.log(f"classification/{phase}/accuracy", acc, sync_dist=True)
+
+    return ret
+#===================== loss =====================
+
+def set_schedule(module):
+    lr = module.hparams.config["learning_rate"]
+    wd = module.hparams.config["weight_decay"]
 
     no_decay = [
         "bias",
@@ -36,16 +171,16 @@ def set_schedule(pl_module):
         "norm2.weight",
     ]
     head_names = ["regression_head", "classification_head"]
-    lr_mult = pl_module.hparams.config["lr_mult"]
-    end_lr = pl_module.hparams.config["end_lr"]
-    decay_power = pl_module.hparams.config["decay_power"]
-    optim_type = pl_module.hparams.config["optim_type"]
+    lr_mult = module.hparams.config["lr_mult"]
+    end_lr = module.hparams.config["end_lr"]
+    decay_power = module.hparams.config["decay_power"]
+    optim_type = module.hparams.config["optim_type"]
 
     optimizer_grouped_parameters = [
         {
             "params": [
                 p
-                for n, p in pl_module.named_parameters()
+                for n, p in module.named_parameters()
                 if not any(nd in n for nd in no_decay)  # not within no_decay
                 and not any(bb in n for bb in head_names)  # not within head_names
             ],
@@ -55,7 +190,7 @@ def set_schedule(pl_module):
         {
             "params": [
                 p
-                for n, p in pl_module.named_parameters()
+                for n, p in module.named_parameters()
                 if any(nd in n for nd in no_decay)  # within no_decay
                 and not any(bb in n for bb in head_names)  # not within head_names
             ],
@@ -65,7 +200,7 @@ def set_schedule(pl_module):
         {
             "params": [
                 p
-                for n, p in pl_module.named_parameters()
+                for n, p in module.named_parameters()
                 if not any(nd in n for nd in no_decay)  # not within no_decay
                 and any(bb in n for bb in head_names)  # within head_names
             ],
@@ -75,7 +210,7 @@ def set_schedule(pl_module):
         {
             "params": [
                 p
-                for n, p in pl_module.named_parameters()
+                for n, p in module.named_parameters()
                 if any(nd in n for nd in no_decay) and any(bb in n for bb in head_names)
                 # within no_decay and head_names
             ],
@@ -93,17 +228,17 @@ def set_schedule(pl_module):
     elif optim_type == "sgd":
         optimizer = torch.optim.SGD(optimizer_grouped_parameters, lr=lr, momentum=0.9)
 
-    if pl_module.trainer.max_steps == -1:
-        max_steps = pl_module.trainer.estimated_stepping_batches
+    if module.trainer.max_steps == -1:
+        max_steps = module.trainer.estimated_stepping_batches
     else:
-        max_steps = pl_module.trainer.max_steps
+        max_steps = module.trainer.max_steps
 
-    warmup_steps = pl_module.hparams.config["warmup_steps"]
-    if isinstance(pl_module.hparams.config["warmup_steps"], float):
+    warmup_steps = module.hparams.config["warmup_steps"]
+    if isinstance(module.hparams.config["warmup_steps"], float):
         warmup_steps = int(max_steps * warmup_steps)
 
     print(
-        f"max_epochs: {pl_module.trainer.max_epochs} | max_steps: {max_steps} | warmup_steps : {warmup_steps} "
+        f"max_epochs: {module.trainer.max_epochs} | max_steps: {max_steps} | warmup_steps : {warmup_steps} "
         f"| weight_decay : {wd} | decay_power : {decay_power}"
     )
 
